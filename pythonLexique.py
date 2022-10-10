8aee9d93bcf69545a4f834c7f9afdda63dc27015
Theo Lalande <theo.lalande@gmail.com>
2022-10-10 12:23:06 +0200

    ajout ligne

--- pythonLexique.py
+++ pythonLexique.py
@@ -1,3 +1,5 @@
+
+# Notes du 10 oct 2022
 """ Commentaire multi ligne"""
 
 print(2**1024)  #** = puissance
@@ -6,9 +8,16 @@ ma_variable_1 = 1
 ma_variable_2 = 2
 ma_variable_1 ma_variable_2 = ma_variable_2 ma_variable_1  #Permutation
 
-typeof(ma_variable_1) #Retourne le type  ma _variable_1
+type(ma_variable_1) #Retourne le type  ma _variable_1
 
 ma_chaine_1 = """Chaine 1"""
 ma_chaine_2 = '''Chaine 2'''
-ma_chaine_1 = "Chaine 1"
-ma_chaine_1 = 'Chaine 1'
+ma_chaine_1 = "Chaine 3"
+ma_chaine_1 = 'Chaine 4'
+
+entier = 20
+bin(entier) #Retourne 20 en base 2
+oct(entier) #Retourne 20 en base 8
+hex(entier) #retourne 20 en base 16
+
+help(print) #Permet d'afficher la doc de la fonction print
