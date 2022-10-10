c148731634c0e4db535e4172e4a7f3a49e7d8226
Theo Lalande <theo.lalande@gmail.com>
2022-10-10 12:06:54 +0200

    ajout ligne

--- pythonLexique.py
+++ pythonLexique.py
@@ -7,3 +7,8 @@ ma_variable_2 = 2
 ma_variable_1 ma_variable_2 = ma_variable_2 ma_variable_1  #Permutation
 
 typeof(ma_variable_1) #Retourne le type  ma _variable_1
+
+ma_chaine_1 = """Chaine 1"""
+ma_chaine_2 = '''Chaine 2'''
+ma_chaine_1 = "Chaine 1"
+ma_chaine_1 = 'Chaine 1'
