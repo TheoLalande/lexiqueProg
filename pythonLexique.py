c8b21828e2eacab7613c3a7b597fc61ca984ab3d
Theo Lalande <theo.lalande@gmail.com>
2022-10-10 12:02:40 +0200

    ajout ligne

--- pythonLexique.py
+++ pythonLexique.py
@@ -1,4 +1,9 @@
 """ Commentaire multi ligne"""
-print(2**1024)  # ** = puissance
 
+print(2**1024)  #** = puissance
 
+ma_variable_1 = 1
+ma_variable_2 = 2
+ma_variable_1 ma_variable_2 = ma_variable_2 ma_variable_1  #Permutation
+
+typeof(ma_variable_1) #Retourne le type  ma _variable_1
